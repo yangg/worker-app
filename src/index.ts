e1a2608cb4be33e5bbbc8bdbc9242d5bcd5599d7
Brook <uedsky@gmail.com>
2024-05-17 16:50:53 +0800

    feat: r2 api

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,51 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+function checkPermission(request: Request, env: Env, key: string) {
+  switch (request.method) {
+    case 'PUT':
+    case 'DELETE':
+      return request.headers.get('TOKEN') === env.AUTH_KEY_SECRET;
+    default:
+      return true;
+  }
+}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const url = new URL(request.url);
+    const key = url.pathname.slice(1);
+
+    if (!checkPermission(request, env, key)) {
+      return Response.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    switch (request.method) {
+      case 'GET':
+      case 'HEAD':
+        const object = await env.BUCKET_APP.get(key);
+
+        if (object === null) {
+          return Response.json({ message: 'Object Not Found!' }, { status: 404 });
+        }
+
+        const headers = new Headers();
+        object.writeHttpMetadata(headers);
+        headers.set('etag', object.httpEtag);
+
+        return new Response(object.body, {
+          headers
+        });
+      case 'PUT':
+        await env.BUCKET_APP.put(key, request.body);
+        return Response.json({ message: `Put ${key} successfully!` });
+      case 'DELETE':
+        await env.BUCKET_APP.delete(key);
+        return Response.json({ message: 'Deleted!' });
+      default:
+        return Response.json({ message: 'Method Not Allowed!' }, {
+          status: 405,
+          headers: {
+            Allow: 'GET, HEAD, PUT, DELETE'
+          }
+        });
+    }
+  }
 };
