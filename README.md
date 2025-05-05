d5d11b6b31fb61d65c7ee3b4aca34b64c6ddc818
Brook <yangguang@jeelyton.com>
2025-05-05 13:00:17 +0800

    feat: add code

---
+++ README.md
@@ -0,0 +1,11 @@
+https://developers.cloudflare.com/workers/get-started/guide/
+
+```sh
+pnpm create cloudflare@latest my-first-worker
+
+
+npx wrangler secret put AUTH_KEY_SECRET
+
+
+npm run cf-typegen / dev / deploy
+```
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,106 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * https://developers.cloudflare.com/workers/get-started/guide/
  */
 
+function checkPermission(request: Request, env: Env, key: string) {
+	switch (request.method) {
+		case 'POST':
+		case 'PUT':
+		case 'DELETE':
+		case 'PATCH':
+			return request.headers.get('TOKEN') === env.AUTH_KEY_SECRET;
+		default:
+			return true;
+	}
+}
+
+async function uploadFile(env: Env, key: string, file: ReadableStream | Blob, contentType: string) {
+	const meta =  {
+		contentType
+	}
+	await env.BUCKET_APP.put(key, file, {
+		httpMetadata: meta
+	});
+	return { message: `${key} Uploaded!`, meta }
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const key = url.pathname.slice(1);
+
+		if (!checkPermission(request, env, key)) {
+			return Response.json({ message: 'Unauthorized' }, { status: 401 });
+		}
+
+		// Check cache first
+		const cache = caches.default;
+		const cacheKey = new Request(url.toString(), request);
+
+		let res
+		switch (request.method) {
+			case 'GET':
+			case 'HEAD':
+				break
+			case 'PUT': {
+				const file = request.body;
+				if (!file) {
+					return Response.json({ message: 'No file provided' }, { status: 400 });
+				}
+				res = await uploadFile(env, key, file, request.headers.get('content-type') ?? '')
+				break
+			}
+			case 'POST':
+				const formData = await request.formData();
+				const file = formData.get('file');
+				if (!(file  && file instanceof File)) {
+					return Response.json({ message: 'No file provided' }, { status: 400 });
+				}
+				res = await uploadFile(env, key, file.stream(), file.type)
+				break
+			case 'DELETE':
+				await env.BUCKET_APP.delete(key);
+				res = { message: `${key} deleted!` }
+				break
+			case 'PATCH':
+				res = { message: `Cache cleared for ${key}`}
+				break
+			default:
+				return Response.json({ message: 'Method not allowed' }, { status: 405 });
+		}
+		if(res) {
+			await cache.delete(cacheKey);
+			return Response.json(res)
+		}
+
+		let response = await cache.match(cacheKey);
+
+		if (response) {
+			return response;
+		}
+
+		// Not in cache, fetch from R2
+		const object = await env.BUCKET_APP.get(key);
+
+		if (!object) {
+			return Response.json({ message: "Object not found"}, { status: 404 });
+		}
+
+		const headers = new Headers();
+		headers.set("Content-Type", object.httpMetadata?.contentType || "application/octet-stream");
+		headers.set("Cache-Control", "max-age=300");
+		// headers.set("Cache-Control", "public, max-age=31536000, immutable");
+		if (object.etag) {
+			headers.set("ETag", object.etag);
+		}
+
+		response = new Response(object.body, {
+			headers,
+			status: 200
+		});
+
+		// Store in cache
+		ctx.waitUntil(cache.put(cacheKey, response.clone()));
+		return response;
+	}
 } satisfies ExportedHandler<Env>;
